486f8cc1d23de0bba33d3c95eda252094a143d37
KoaxialKable <KoaxialKable@gmail.com>
2015-07-22 00:20:42 -0600

    Separated script into its own .js file

---
+++ build/.module-cache/cfcd06dbdbf4bd86d082d02f44bc89a9cfb4d435.js
@@ -0,0 +1,119 @@
+var TodoRow = React.createClass({displayName: "TodoRow",
+  getInitialState: function() {
+    return {id: this.props.id, editMode: this.props.focused}
+  },
+
+
+  editClick: function() {
+    this.props.editClick(this);
+  },
+
+  updateClick: function() {
+    this.props.updateClick(this);
+  },
+
+  deleteClick: function() {
+    this.props.deleteClick(this);
+  },
+
+  render: function() {
+    var editMode = this.props.focused;
+    if (editMode) {
+      return (
+        React.createElement("li", {key: this.props.id, className: "TodoRowEdit"}, 
+          React.createElement("input", {type: "text", ref: "txtUpdate", defaultValue: this.props.text}), 
+          React.createElement("button", {type: "button", ref: "btnUpdate", onClick: this.updateClick}, "Update"), 
+          React.createElement("button", {type: "button", ref: "btnCancel", onClick: this.editClick}, "Cancel")
+        )
+      );
+    } else {
+      return (
+        React.createElement("li", {key: this.props.id, className: "TodoRow"}, 
+          React.createElement("span", null, this.props.text), 
+          React.createElement("button", {type: "button", ref: "btnEdit", onClick: this.editClick}, "Edit"), 
+          React.createElement("button", {type: "button", ref: "btnDelete", onClick: this.deleteClick}, "Delete")
+        )
+      );
+    }
+  }
+});
+
+
+
+
+var TodoList = React.createClass({displayName: "TodoList",
+  getInitialState: function() {
+    return {focusID: null, list: this.props.list};
+  },
+
+
+  editClick: function(childComponent) {
+    var childID = childComponent.state.id;
+    if (this.state.focudID == childID) {
+      // already in edit mode: cancel
+      this.setState({focusID: -1});
+    } else {
+      // enter edit mode
+      this.setState({focusID: childID});
+    };
+    
+  },
+
+  updateClick: function(childComponent) {
+    var newText = childComponent.refs.txtUpdate.getDOMNode().value;
+    var idToUpdate = childComponent.state.id;
+    var list = this.state.list;
+    list[idToUpdate].text = newText;
+    this.setState({focusID: null, list: list})
+  },
+
+  deleteClick: function(childComponent) {
+    var idToDelete = childComponent.state.id;
+    var list = this.state.list;
+    list.splice(idToDelete, 1);
+    this.setState({list: list})
+  },
+
+  addNewItem: function() {
+    var list = this.state.list;
+    list.push({text: "New Item"})
+    var focusOn = list.length-1;
+    this.setState({list: list, focusID: focusOn})
+  },
+
+
+  render: function() {
+    var focusID = this.state.focusID;
+    var listNodes = this.props.list.map(function (todoItem, i) {
+      return (
+        React.createElement(TodoRow, {
+          text: todoItem.text, 
+          focused: i == focusID, 
+          id: i, 
+          editClick: this.editClick, 
+          updateClick: this.updateClick, 
+          deleteClick: this.deleteClick}
+        )
+      );
+    }, this);
+    return (
+      React.createElement("div", {className: "TodoList"}, 
+        React.createElement("h3", null, "To do:"), 
+        React.createElement("ul", null, 
+          listNodes, 
+          React.createElement("li", {key: "add"}, React.createElement("button", {type: "button", onClick: this.addNewItem}, "Add"))
+        )
+
+      )
+    );
+  }
+});
+
+var initialList = [
+  {text: "Walk dog"},
+  {text: "Do laundry"},
+  {text: "Do dishes"}
+];
+
+
+React.render(React.createElement(TodoList, {list: initialList}), document.getElementById('content'));
---
+++ src/todolist.js
@@ -0,0 +1,119 @@
+var TodoRow = React.createClass({
+  getInitialState: function() {
+    return {id: this.props.id, editMode: this.props.focused}
+  },
+
+
+  editClick: function() {
+    this.props.editClick(this);
+  },
+
+  updateClick: function() {
+    this.props.updateClick(this);
+  },
+
+  deleteClick: function() {
+    this.props.deleteClick(this);
+  },
+
+  render: function() {
+    var editMode = this.props.focused;
+    if (editMode) {
+      return (
+        <li key={this.props.id} className="TodoRowEdit">
+          <input type="text" ref="txtUpdate" defaultValue={this.props.text}/>
+          <button type="button" ref="btnUpdate" onClick={this.updateClick}>Update</button>
+          <button type="button" ref="btnCancel" onClick={this.editClick}>Cancel</button>
+        </li>
+      );
+    } else {
+      return (
+        <li key={this.props.id} className="TodoRow">
+          <span>{this.props.text}</span>
+          <button type="button" ref="btnEdit" onClick={this.editClick}>Edit</button>
+          <button type="button" ref="btnDelete" onClick={this.deleteClick}>Delete</button>
+        </li>
+      );
+    }
+  }
+});
+
+
+
+
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {focusID: null, list: this.props.list};
+  },
+
+
+  editClick: function(childComponent) {
+    var childID = childComponent.state.id;
+    if (this.state.focudID == childID) {
+      // already in edit mode: cancel
+      this.setState({focusID: null});
+    } else {
+      // enter edit mode
+      this.setState({focusID: childID});
+    };
+    
+  },
+
+  updateClick: function(childComponent) {
+    var newText = childComponent.refs.txtUpdate.getDOMNode().value;
+    var idToUpdate = childComponent.state.id;
+    var list = this.state.list;
+    list[idToUpdate].text = newText;
+    this.setState({focusID: null, list: list})
+  },
+
+  deleteClick: function(childComponent) {
+    var idToDelete = childComponent.state.id;
+    var list = this.state.list;
+    list.splice(idToDelete, 1);
+    this.setState({list: list})
+  },
+
+  addNewItem: function() {
+    var list = this.state.list;
+    list.push({text: "New Item"})
+    var focusOn = list.length-1;
+    this.setState({list: list, focusID: focusOn})
+  },
+
+
+  render: function() {
+    var focusID = this.state.focusID;
+    var listNodes = this.props.list.map(function (todoItem, i) {
+      return (
+        <TodoRow
+          text={todoItem.text}
+          focused={i == focusID}
+          id={i}
+          editClick={this.editClick}
+          updateClick={this.updateClick}
+          deleteClick={this.deleteClick}
+        />
+      );
+    }, this);
+    return (
+      <div className="TodoList">
+        <h3>To do:</h3>
+        <ul>
+          {listNodes}
+          <li key="add"><button type="button" onClick={this.addNewItem}>Add</button></li>
+        </ul>
+
+      </div>
+    );
+  }
+});
+
+var initialList = [
+  {text: "Walk dog"},
+  {text: "Do laundry"},
+  {text: "Do dishes"}
+];
+
+
+React.render(<TodoList list={initialList} />, document.getElementById('content'));
